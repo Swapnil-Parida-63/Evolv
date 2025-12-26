8581677e0f4b7e74142184f2c5eb3eb458d65d02
Swapnil-Parida63 <swapnilparida7@gmail.com>
2025-12-26 21:58:56 +0530

    minor modification

--- client/server/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { analyzeResume } from "../controllers/aiController.js";
-const router = express.Router();
-
-router.post("/analyze", analyzeResume);
-
-export default router;
